b0723a9a9f90cf60aabf0bef7a3e83613d69ec8a
Marcelino Guerrero <mguerreroh@yahoo.com>
2012-03-23 18:46:18 -0500

    Actualizacion de valuidacion de fecha

--- POO-RETO01/Reto01/test/controladoras/AdmFacturaTest.java
+++ POO-RETO01/Reto01/test/controladoras/AdmFacturaTest.java
@@ -6,6 +6,7 @@ package controladoras;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import java.lang.String;
 
 /**
  *
@@ -42,7 +43,7 @@ public class AdmFacturaTest {
     @Test
     public void siNoingresoMontoVentaDebeDarError(){
         assertFalse(admFactura.verificarMontoVenta(null));
-        System.out.println("La Fecha no debe estar vacia");
+        System.out.println("El Monto de Venta no debe estar vacio");
     }
 
     @Test
@@ -52,5 +53,32 @@ public class AdmFacturaTest {
         assertFalse(admFactura.verificarMontoVentaMayorCero(-1.00));
         System.out.println("El montoVenta no debe ser menor a Cero");
     }
+    
+    @Test
+    public void siIngresaFechaErradaDebeDarError(){
+        assertFalse(admFactura.verificarFechaReal("01-31-2012"));
+        //assertTrue(admFactura.verificarFechaReal("01-81-2012"));
+        System.out.println("La fecha es incorrecta");
+    }
+    
+    @Test
+    public void verificaFecha(){
+        assertFalse(!=admFactura.validarFecha("2012-12-32"));
+        System.out.println("La fecha es incorrecta");
+    }
+    
+
+    /*
+    @Test
+    public void alumnosdebeTenerUnaCalificacion(){
+        String dni = "09534044";
+        String nombre = "Marcelino";
+        String apellido = "tradicional";
+        Alumno alumno = new Alumno(codigo, nombre, modalidad);
+        Calificacion calificacion = alumno.getCalificacion();
+        Assert.assertNotNull(calificacion);
+    }
+    */
+     
 
 }
